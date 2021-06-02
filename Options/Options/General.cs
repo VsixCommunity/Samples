bfe25fffced38d1282da8dd832e942b5f6c094f4
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-06-02 10:53:15 -0700

    Added Options sample

---
+++ Options/Options/General.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using Community.VisualStudio.Toolkit;
+
+namespace Options
+{
+    internal partial class OptionsProvider
+    {
+        // Register the options with these attributes on your package class:
+        // [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "MyExtension", "General", 0, 0, true)]
+        // [ProvideProfile(typeof(OptionsProvider.GeneralOptions), "MyExtension", "General", 0, 0, true)]
+        public class GeneralOptions : BaseOptionPage<General> { }
+    }
+
+    public class General : BaseOptionModel<General>
+    {
+        [Category("My category")]
+        [DisplayName("My Option")]
+        [Description("An informative description.")]
+        [DefaultValue(true)]
+        public bool MyOption { get; set; } = true;
+
+        [Category("My category")]
+        [DisplayName("My Enum")]
+        [Description("Select the value you want from the list.")]
+        [DefaultValue(Numbers.First)]
+        [TypeConverter(typeof(EnumConverter))]
+        public Numbers MyEnum { get; set; } = Numbers.First;
+    }
+
+    public enum Numbers
+    {
+        First,
+        Second,
+        Third,
+    }
+}
---
+++ Options/OptionsPackage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Options
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.OptionsString)]
+    [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "MyExtension", "General", 0, 0, true)]
+    [ProvideProfile(typeof(OptionsProvider.GeneralOptions), "MyExtension", "General", 0, 0, true)]
+    public sealed class OptionsPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            General.Saved += OnSettingsSaved;
+
+        }
+
+        private void OnSettingsSaved(object sender, General e)
+        {
+           
+        }
+
+        private void InteractWithSettings()
+        {
+            // read settings
+            var number = General.Instance.MyEnum;
+
+            // write settings
+            General.Instance.MyEnum = Numbers.Second;
+            General.Instance.Save();
+        }
+
+        private async Task InteractWithSettingsAsync()
+        {
+            // read settings
+            var general = await General.GetLiveInstanceAsync();
+            var number = general.MyEnum;
+            
+            // write settings
+            general.MyEnum = Numbers.Second;
+            await general.SaveAsync();
+        }
+    }
+}
